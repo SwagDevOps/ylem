53616142b89ac69dc4295d41165194ae9e855c71
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-05-31 08:18:21 +0200

    helper/config (lib) introduced

---
+++ lib/ylem/helper/config.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'ylem/helper'
+require 'ylem/concern/helper'
+require 'etc'
+require 'pathname'
+
+# Config helper
+class Ylem::Helper::Config
+  include Ylem::Concern::Helper
+
+  # Default config
+  #
+  # @return [Hash]
+  def defaults
+    {
+      log_file: Pathname.new('/').join('var', 'log', "#{progname}.log"),
+      scripts_dir: Pathname.new(Etc.sysconfdir).join(progname, 'scripts'),
+      env_file: Pathname.new(Etc.sysconfdir).join('environment')
+    }
+  end
+
+  # Default config file
+  #
+  # @return [Pathname]
+  def default_file
+    Pathname.new(Etc.sysconfdir).join(progname, 'config.yml')
+  end
+
+  # Parse comfig (yaml) file
+  #
+  # @param [Pathname|String] filepath
+  # @return [Hash]
+  def parse(filepath = default_file)
+    filepath = Pathname.new(filepath)
+
+    if filepath == default_file and !filepath.exist?
+      parsed = { }
+    else
+      parsed = helper.get('yaml').parse_file(filepath)
+    end
+
+    defaults.merge(parsed)
+  end
+
+  protected
+
+  # @return [String]
+  def progname
+    helper.get('system').progname
+  end
+end
