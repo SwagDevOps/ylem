2a3be1fee56907d8302b5eb1c6a488e0282315eb
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-06-01 08:38:30 +0200

    helper/config_spec (spec) new examples

--- spec/ylem/helper/config_spec.rb
+++ spec/ylem/helper/config_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'ylem/helper/config'
+require 'pathname'
 
 describe Ylem::Helper::Config do
   {
@@ -9,6 +10,18 @@ describe Ylem::Helper::Config do
     parse_file: 1,
     parse: 1,
   }.each do |method, n|
-    it { expect(described_class.new).to respond_to(method).with(n).arguments }
+    it { expect(subject).to respond_to(method).with(n).arguments }
+  end
+
+  context '#default_file' do
+    it { expect(subject.default_file).to be_a(Pathname) }
+
+    # default value (example);
+    # ``#<Pathname:/etc/progname/config.yml>``
+    it do
+      reg = /^\/etc\/(rake|rspec)\/config\.yml$/
+
+      expect(subject.default_file.to_s).to match(reg)
+    end
   end
 end
