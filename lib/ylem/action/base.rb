8c1651f02f5d35e66fded42ba12a6d387cf41e61
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-06-04 19:52:03 +0200

    action/base (lib) major changes

--- lib/ylem/action/base.rb
+++ lib/ylem/action/base.rb
@@ -3,13 +3,33 @@
 require 'ylem/action'
 require 'active_support/descendants_tracker'
 
-# Base action
+# @abstract Subclass and override {#execute} to implement
+#           a custom ``Action`` class.
 class Ylem::Action::Base
+  # Config as seen in ``Ylem::Helper::Config``
+  #
+  # @see Ylem::Helper::Config
+  # @return [Hash]
   attr_reader :config
+
+  # Return code (to be used as status code by command line)
+  #
+  # @return [Fixnum]
+  attr_reader :retcode
+
   extend ActiveSupport::DescendantsTracker
 
-  # @param [Hash]
-  def initialize(config = {})
+  # @param [Hash] config
+  def initialize(config)
     @config = config
+    @retcode = Errno::NOERROR::Errno
+  end
+
+  # Execute action
+  # @note This method SHOULD be implemented by descendant classes
+  #
+  # @return [self]
+  def execute
+    self
   end
 end
