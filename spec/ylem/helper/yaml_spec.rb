2a2dcca87ba60aecf30d1d02eda10ad32ac7a307
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-06-08 19:41:23 +0200

    helper/yaml_spec (spec) introduced

---
+++ spec/ylem/helper/yaml_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'ylem/helper/yaml'
+require 'pathname'
+
+local = {
+  sample_config: "#{SPEC_DIR}/samples/config.yml",
+}
+
+describe Ylem::Helper::Yaml do
+  {
+    parse: [1],
+    parse_file: [1],
+  }.each do |method, counts|
+    counts.each do |n|
+      it { expect(subject).to respond_to(method).with(n).arguments }
+    end
+  end
+
+  context "#parse_file('#{local[:sample_config]}')" do
+    let(:parsed) { subject.parse_file(local[:sample_config]) }
+
+    it { expect(parsed).to be_a(Hash) }
+
+    it { expect(parsed).to have_all_symbol_keys }
+  end
+end
