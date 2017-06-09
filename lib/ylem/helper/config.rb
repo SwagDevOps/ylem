13554356bf9db0ad195f94fdef26d8cffd497c46
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-06-09 19:16:02 +0200

    helper/config_reader (lib) introduced

--- lib/ylem/helper/config.rb
+++ lib/ylem/helper/config.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 require 'ylem/helper'
-require 'ylem/concern/helper'
-require 'etc'
-require 'pathname'
+require 'ylem/helper/config_reader'
 
 # Config helper
-class Ylem::Helper::Config
-  include Ylem::Concern::Helper
-
+class Ylem::Helper::Config < Ylem::Helper::ConfigReader
   # Default config
   #
   # @return [Hash]
@@ -18,52 +14,5 @@ class Ylem::Helper::Config
       scripts_dir: Pathname.new(Etc.sysconfdir).join(progname, 'scripts'),
       env_file: Pathname.new(Etc.sysconfdir).join('environment')
     }
-  end
-
-  # Default config file
-  #
-  # @return [Pathname]
-  def default_file
-    Pathname.new(Etc.sysconfdir).join(progname, 'config.yml')
-  end
-
-  def default_file?(filepath)
-    Pathname.new(filepath) == default_file
-  end
-
-  # Parse comfig (yaml) file
-  #
-  # @param [Pathname|String] filepath
-  # @return [Hash]
-  def parse_file(filepath = default_file)
-    filepath = Pathname.new(filepath)
-
-    if default_file?(filepath) and !filepath.exist?
-      return defaults
-    end
-
-    parse(filepath.read)
-  end
-
-  # Parse string content (yaml) merging with defauts
-  #
-  # @return [Hash|Array]
-  def parse(content)
-    parsed = helper.get('yaml').parse(content)
-
-    # @todo raise explicit exception before merge
-    result = defaults.merge(parsed)
-    defaults.each do |k, v|
-      result[k] = Pathname.new(result[k]) if result[k] and v.is_a?(Pathname)
-    end
-
-    result
-  end
-
-  protected
-
-  # @return [String]
-  def progname
-    helper.get('system').progname
   end
 end
---
+++ lib/ylem/helper/config_reader.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'ylem/helper'
+require 'ylem/concern/helper'
+require 'etc'
+require 'pathname'
+
+# Helper intended to read config files
+#
+# Mostly based on ``Ylem::Helper::Yaml``
+class Ylem::Helper::ConfigReader
+  include Ylem::Concern::Helper
+
+  # Default config
+  #
+  # @return [Hash]
+  def defaults
+    {}
+  end
+
+  # Default config file
+  #
+  # @return [Pathname]
+  def default_file
+    Pathname.new(Etc.sysconfdir).join(progname, 'config.yml')
+  end
+
+  def default_file?(filepath)
+    Pathname.new(filepath) == default_file
+  end
+
+  # Parse comfig (yaml) file
+  #
+  # @param [Pathname|String] filepath
+  # @return [Hash]
+  def parse_file(filepath = default_file)
+    filepath = Pathname.new(filepath)
+
+    if default_file?(filepath) and !filepath.exist?
+      return defaults
+    end
+
+    parse(filepath.read)
+  end
+
+  # Parse string content (yaml) merging with defauts
+  #
+  # @return [Hash|Array]
+  def parse(content)
+    parsed = helper.get('yaml').parse(content)
+
+    # @todo raise explicit exception before merge
+    result = defaults.merge(parsed)
+    defaults.each do |k, v|
+      result[k] = Pathname.new(result[k]) if result[k] and v.is_a?(Pathname)
+    end
+
+    result
+  end
+
+  protected
+
+  # @return [String]
+  def progname
+    helper.get('system').progname
+  end
+end
