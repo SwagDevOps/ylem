7992ca9c254aa555587ba05ac0932689f0b55fcc
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-06-20 08:40:55 +0200

    action/dump (lib) documentation added + minor changes

--- lib/ylem/action/dump.rb
+++ lib/ylem/action/dump.rb
@@ -3,6 +3,13 @@
 require 'ylem/action/base'
 require 'json'
 
+# Action intended to display parsed (and derivated) config
+#
+# Derived config is displayed as JSON encoded string
+# and cand be accessed using dot notation
+#
+# @see Ylem.Helper.Config.Decorator
+# @see https://github.com/adsteel/hash_dot
 class Ylem::Action::Dump < Ylem::Action::Base
   # Execute action
   #
@@ -22,16 +29,29 @@ class Ylem::Action::Dump < Ylem::Action::Base
 
   protected
 
+  # Dumpable content
+  #
+  # return [Hash|Array]
   def dumpable
     dumpable = config
-    sections = options[:key].to_s.split('.')
 
     unless sections.empty?
       sections.each do |section|
-        dumpable = dumpable.public_send(section)
+        args = [section, []]
+        # args = ['[]', /^[0-9]{1,}$/ =~ section ? section.to_i : section] \
+        # if dumpable.is_a?(Array)
+
+        dumpable = dumpable.public_send(*args)
       end
     end
 
     dumpable
+  end
+
+  # Get requested section(s), ``options`` based
+  #
+  # @return [Hash<String>]
+  def sections
+    options[:key].to_s.split('.')
   end
 end
