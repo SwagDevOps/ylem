ea9303c56f4715fae23c0f0974d70e393b812be8
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-06-29 19:22:24 +0200

    service (lib) introduced

---
+++ lib/ylem/service.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'ylem'
+require 'ylem/concern/helper'
+require 'singleton'
+
+# Provides access to helper classes
+class Ylem::Service
+  include ::Singleton
+  include Ylem::Concern::Helper
+
+  # @param [String|Symbol] name
+  # @return [Object]
+  #
+  # @raise [NotImplementedError]
+  def get(name)
+    inflector = helper.get(:inflector)
+    name = name.to_sym
+
+    return items[name] if items[name]
+
+    begin
+      @items[name] = inflector.resolve("ylem/service/#{name}").new
+    rescue LoadError
+      raise NotImplementedError, "service not loadable: #{name}"
+    end
+  end
+
+  class << self
+    def method_missing(method, *args)
+      super unless respond_to_missing?(method)
+
+      instance.public_send(method, *args)
+    end
+
+    def respond_to_missing?(method, include_all = false)
+      instance.respond_to?(method, include_all)
+    end
+  end
+
+  protected
+
+  attr_reader :items
+
+  def initialize
+    @items = {}
+  end
+end
