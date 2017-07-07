88ff0bd3bfd56991a678b67e76989378a3189716
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-07 20:06:15 +0200

    cli/start (lib) minor changes + documentation (minimal)

--- lib/ylem/cli/start.rb
+++ lib/ylem/cli/start.rb
@@ -2,13 +2,14 @@
 
 require 'ylem/cli/base'
 
+# CLI ``start`` command class
 class Ylem::Cli::Start < Ylem::Cli::Base
-  # Read config
+  # Get config
   #
   # additional arguments are added to ``config`` as ``:command``
   #
   # @return [Hash]
   def config
-    super.merge({command: arguments})
+    super.merge(command: arguments)
   end
 end
