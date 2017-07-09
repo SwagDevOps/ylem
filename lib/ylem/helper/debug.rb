7aaa32f52ea69cbacf3104f48683b018a77a0cef
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-09 21:17:13 +0200

    helper/debug (lib) major changes, improvements and bugfixes

--- lib/ylem/helper/debug.rb
+++ lib/ylem/helper/debug.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 require 'ylem/helper'
+require 'ylem/concern/helper'
+require 'tty/screen'
 
+# Provides color printer automagically
 class Ylem::Helper::Debug
+  include Ylem::Concern::Helper
+
   # Outputs obj to out in pretty printed format of width columns in width.
   #
   # If out is omitted, ``STDOUT`` is assumed.
@@ -13,16 +18,44 @@ class Ylem::Helper::Debug
   # @param [Fixnum] width
   # @see http://ruby-doc.org/stdlib-2.2.0/libdoc/pp/rdoc/PP.html
   def dump(obj, out = STDOUT, width = nil)
+    width ||= TTY::Screen.new.width || 79
+    printer = out.isatty ? 0 : 1
+
+    printers[printer].pp(obj, out, width)
+  end
+
+  # Get printers
+  #
+  # @return [Array]
+  def printers
+    printers_load
+
+    [
+      proc do
+        target = 'Pry::ColorPrinter'
+
+        Kernel.const_defined?(target) ? target : 'PP'
+      end.call,
+      'PP',
+    ].map { |n| helper.get('inflector').constantize(n) }
+  end
+
+  protected
+
+  # Load printers requirements (on demand)
+  def printers_load
     require 'pp'
+
     begin
       require 'coderay'
       require 'pry/color_printer'
     rescue LoadError => e
-      warn('%s: %s' % [caller[0], e.message])
+      # rubocop:disable Performance/Caller
+      warn('%s: %s' % [caller[0], e.message]) if @warned.nil?
+      # rubocop:enable Performance/Caller
+      @warned = true
     end
 
-    colorable = (out.isatty and Kernel.const_defined?('Pry::ColorPrinter'))
-
-    (colorable ? Pry::ColorPrinter : PP).pp(obj, out, width)
+    self
   end
 end
