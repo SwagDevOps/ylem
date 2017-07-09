6e1d449d83d8f36f40a99ca12381a6223ac09270
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-09 15:06:31 +0200

    type/script (lib) gains reponsability to execute itself

--- lib/ylem/type/script.rb
+++ lib/ylem/type/script.rb
@@ -2,13 +2,39 @@
 
 require 'ylem/type'
 require 'pathname'
+require 'ylem/concern/helper'
 
 # Describe a script file
 #
 # mostly a simple specialization based on ``Pathname``
 class Ylem::Type::Script < Pathname
+  include Ylem::Concern::Helper
+
+  # Denote is a script (executable file)
+  #
   # @return [Boolean]
   def script?
     self.file? and self.executable? and self.readable?
+  end
+
+  # Execute itself
+  #
+  # Script is responsible to execute itself, executing a non-executable or
+  # inexistent script SHOULD return a ``127`` status
+  #
+  # @param [Hash] options
+  # @option options [Ylem::Service::Logger] :logger
+  # @option options [Symbol|String] :as
+  #   method applied on instance (example: ``:basename``)
+  #   identify itself with ``logger``
+  #
+  # @return [Fixnum]
+  def execute(options = {})
+    subprocess = helper.get('subprocess')
+    # options processing
+    as = options[:as] ? self.public_send(options.fetch(:as)) : self
+    logger = options[:logger]&.as(as)
+
+    subprocess.run([self], logger: logger).to_i
   end
 end
