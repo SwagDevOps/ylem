23c213a9d94dcb597012db8fa2f8aadf64aef629
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-16 14:40:53 +0200

    config_defaults (spec/factories) %r{}

--- spec/factories/config_defaults.rb
+++ spec/factories/config_defaults.rb
@@ -4,13 +4,10 @@
 FactoryGirl.define do
   factory :config_defaults, class: FactoryStruct do
     # Patterns used to match config file default values
-    #
-    # rubocop:disable Style/RegexpLiteral
     patterns(
-      'logger.file':      /^\/var\/log\/(rake|rspec)\.log$/,
-      'scripts.path':     /^\/etc\/(rake|rspec)\/scripts$/,
-      'environment.file': /^\/etc\/environment$/
+      'logger.file':      %r{^/var/log/(rake|rspec).log$},
+      'scripts.path':     %r{^/etc/(rake|rspec)/scripts$},
+      'environment.file': %r{^/etc/environment$}
     )
-    # rubocop:enable Style/RegexpLiteral
   end
 end
