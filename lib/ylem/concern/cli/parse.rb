5b1382084c16d906cb2c869c82d12e0c20ec4339
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-18 19:18:56 +0200

    concern/cli/parse (lib) <- concern/cli/parsed_run

---
+++ lib/ylem/concern/cli/parse.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'active_support/concern'
+
+require 'ylem/concern/cli'
+require 'ylem/concern/helper'
+require 'ylem/concern/cli/output'
+
+# Provide a convenient and reusable way to prepare the ``#run``
+#
+# Class using this concern MUST define a ``#parse!`` method
+#
+# ### Sample of use
+#
+# ```{ruby}
+# require 'ylem/concern/cli/parse'
+#
+# class Ylem::Cli
+#   include Ylem::Concern::Cli::Parse
+#   # @return [Fixnum]
+#   def run
+#     parse { do_something }
+#   end
+# end
+# ```
+module Ylem::Concern::Cli::Parse
+  include Ylem::Concern::Helper
+  include Ylem::Concern::Cli::Output
+
+  protected
+
+  # rubocop:disable Metrics/MethodLength
+
+  # Block to run wrapped CLI method
+  #
+  # Parse command line arguments (using OptionParser)
+  # rescue errors to retcode (integer).
+  # When no errors are encountered, given block is executed.
+  #
+  # @param [Boolean] display_error
+  # @yield Execute a given block
+  # @yieldreturn [Integer]
+  # @return [Integer]
+  def parse(display_error = true)
+    error = lambda do |displayable, code = :EINVAL|
+      output(displayable.to_s, to: :stderr) if display_error
+
+      helper.get(:errno).retcode_get(code)
+    end
+
+    begin
+      parse!
+    rescue OptionParser::InvalidOption, OptionParser::InvalidArgument
+      return error.call(parser)
+    rescue OptionParser::MissingArgument => e
+      return error.call(e)
+    end
+
+    block_given? ? yield : 0
+  end
+
+  # rubocop:enable Metrics/MethodLength
+end
