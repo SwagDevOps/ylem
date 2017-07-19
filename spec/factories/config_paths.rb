56ff84633afa599726d15a0452a6a44db28d3851
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-19 20:15:07 +0200

    config_paths (spec/factories) added random path, based on securerandom

--- spec/factories/config_paths.rb
+++ spec/factories/config_paths.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # frozen_string_literal: true
 
+require 'securerandom'
+
 # Paths to success + failure configuration files
 #
 # default config path is a pattern, as progname is dynamic
@@ -9,5 +11,15 @@ FactoryGirl.define do
     success "#{SAMPLES_DIR}/config/success.yml"
     failure "#{SAMPLES_DIR}/config/failure.yml"
     default %r{^/etc/(rake|rspec)/config\.yml$}
+
+    random do
+      [
+        nil,
+        SecureRandom.hex(1),
+        SecureRandom.hex(2),
+        SecureRandom.hex(3),
+        'config.yml'
+      ].join('/')
+    end
   end
 end
