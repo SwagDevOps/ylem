fee85744a75f53e6272a6227465c017afbe7673f
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-24 08:24:37 +0200

    cli/start (lib) documenting

--- lib/ylem/cli/start.rb
+++ lib/ylem/cli/start.rb
@@ -5,6 +5,9 @@ require 'ylem/cli/base'
 # CLI ``start`` command class
 class Ylem::Cli::Start < Ylem::Cli::Base
   class << self
+    # Get summary (short description)
+    #
+    # @return [String]
     def summary
       'Start up'
     end
