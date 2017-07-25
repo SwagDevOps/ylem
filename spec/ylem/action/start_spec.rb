ca13eed20c32abb0a31e7aa3cdd177df8843af95
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-25 19:20:06 +0200

    action/start (spec) minor improvements

--- spec/ylem/action/start_spec.rb
+++ spec/ylem/action/start_spec.rb
@@ -1,23 +1,21 @@
 # frozen_string_literal: true
 
 require 'ylem/action/start'
+require 'fileutils'
 
 # execute a ``success`` action ---------------------------------------
 
 describe Ylem::Action::Start do
-  let(:subject) do
-    config = build(:config_values).success
-
-    described_class.new(config)
-  end
+  let!(:config) { build(:config_values).success }
+  let!(:subject) { described_class.new(config) }
+  before(:example) { FileUtils.rm_f(config.fetch(:'logger.file')) }
 
   context '#execute' do
     it { expect(subject.execute).to be_a(described_class) }
-    context '.retcode' do
-      let(:retcode) { subject.execute.retcode }
 
-      it { expect(retcode).to be_a(Integer) }
-      it { expect(retcode).to be_zero }
+    context '.retcode' do
+      it { expect(subject.execute.retcode).to be_a(Integer) }
+      it { expect(subject.execute.retcode).to be_zero }
     end
   end
 end
