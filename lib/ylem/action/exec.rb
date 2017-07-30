4ae2756a22e470907e22c402fb7ce6f2fb0ae4f2
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-30 20:45:30 +0200

    action/exec (lib) syntactic changes and improvements

--- lib/ylem/action/exec.rb
+++ lib/ylem/action/exec.rb
@@ -10,13 +10,13 @@ require 'ylem/action/base'
 # @see ``Kernel.exec``
 class Ylem::Action::Exec < Ylem::Action::Base
   def execute
-    begin
-      exec(*arguments)
-    rescue Errno::ENOENT => e
-      on_error(e, e.class.name.split('::')[-1])
-    rescue StandardError => e
-      on_error(e)
-    end
+    Kernel.exec(*arguments)
+  rescue Errno::ENOENT => e
+    on_error(e, e.class.name.split('::')[-1])
+  rescue StandardError => e
+    on_error(e)
+  ensure
+    self
   end
 
   protected
@@ -28,7 +28,7 @@ class Ylem::Action::Exec < Ylem::Action::Base
   # @return [self]
   def on_error(e, type = :ENOTRECOVERABLE)
     output(e.message, to: :stderr)
-    self.retcode = helper.get(:errno).retcode_get(type)
+    self.retcode = type
 
     self
   end
