2bbb28e2c4595c995e116f010b2a7a48eadfe3e9
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-30 19:32:15 +0200

    cli/exec (lib) command is mandatory, banner changes

--- lib/ylem/cli/exec.rb
+++ lib/ylem/cli/exec.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
-require 'ylem/cli/start'
+require 'ylem/cli/base'
 
 # CLI ``exec`` command class
-class Ylem::Cli::Exec < Ylem::Cli::Start
+class Ylem::Cli::Exec < Ylem::Cli::Base
   class << self
     # Get summary (short description)
     #
     # @return [String]
     def summary
       'Run the given external command'
+    end
+
+    # @return [String]
+    def banner
+      '%s -- {command}' % super
     end
   end
 end
