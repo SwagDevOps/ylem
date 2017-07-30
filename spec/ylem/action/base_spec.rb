9fbfc8e8f65ce70d42f82c46b7b008576b2824c8
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-30 17:54:27 +0200

    action/base (spec) reflect previous changes #c1137e7

--- spec/ylem/action/base_spec.rb
+++ spec/ylem/action/base_spec.rb
@@ -13,6 +13,7 @@ describe Ylem::Action::Base do
   {
     config:           [0],
     options:          [0],
+    arguments:        [0],
     retcode:          [0],
     'dummy_outputs?': [0],
     execute:          [0],
