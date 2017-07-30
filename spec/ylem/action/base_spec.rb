c4ddd31aaea356ee5eab1ad45f9b2060bf3f5841
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-07-30 20:48:35 +0200

    action/base (spec) success? added

--- spec/ylem/action/base_spec.rb
+++ spec/ylem/action/base_spec.rb
@@ -11,12 +11,13 @@ describe Ylem::Action::Base do
   end
 
   {
-    config:           [0],
-    options:          [0],
-    arguments:        [0],
-    retcode:          [0],
-    'dummy_outputs?': [0],
-    execute:          [0],
+    config:            [0],
+    options:           [0],
+    arguments:         [0],
+    retcode:           [0],
+    'dummy_outputs?':  [0],
+    'success?':        [0],
+    execute:           [0],
   }.each do |method, counts|
     counts.each do |n|
       it { expect(subject).to respond_to(method).with(n).arguments }
