698481f6660a44250ce1e6092ada6d2029297a4f
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-01 21:46:12 +0200

    action/start (spec) minor changes

--- spec/ylem/action/start_spec.rb
+++ spec/ylem/action/start_spec.rb
@@ -8,7 +8,7 @@ require 'fileutils'
 { success: 0, failure: 131 }.each do |config_type, retcode|
   describe Ylem::Action::Start do
     let!(:config) { build(:config_values).public_send(config_type) }
-    let!(:subject) { described_class.new(config) }
+    let!(:subject) { described_class.new(config, [], quiet: true) }
 
     before(:example) do
       logfile = config.fetch(:'logger.file')
@@ -19,15 +19,15 @@ require 'fileutils'
 
     context '#execute' do
       it { expect(subject.execute).to be_a(described_class) }
+    end
 
-      context '.retcode' do
-        it { expect(subject.execute.retcode).to be_a(Integer) }
-        it { expect(subject.execute.retcode).to be(retcode) }
-      end
+    context '#execute.retcode' do
+      it { expect(subject.execute.retcode).to be_a(Integer) }
+      it { expect(subject.execute.retcode).to be(retcode) }
+    end
 
-      context '.config.logger.file' do
-        it { expect(subject.execute.config.logger.file).to be_file }
-      end
+    context '#execute.config.logger.file' do
+      it { expect(subject.execute.config.logger.file).to be_file }
     end
   end
 end
