e41a0c622725b0564b041f4c5984fa128e20f252
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-03 08:58:34 +0200

    helper/system/path (lib) introduced

---
+++ lib/ylem/helper/system/path.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'ylem/helper/system'
+require 'pathname'
+
+# System path helper
+class Ylem::Helper::System::Path
+  # Return system root directory
+  #
+  # @return [Pathname]
+  def rootdir
+    Pathname.new('/')
+  end
+
+  # Return system configuration directory
+  #
+  # @return [Pathname]
+  def sysconfdir
+    rootdir.join('etc')
+  end
+end
