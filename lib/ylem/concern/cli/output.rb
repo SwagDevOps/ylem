33e30a7f10db07eba51b05c38b189e6e77084917
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-06 18:50:59 +0200

    cli/output (concern) ActiveSupport::Concern removed

--- lib/ylem/concern/cli/output.rb
+++ lib/ylem/concern/cli/output.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
-require 'active_support/concern'
-
-require 'ylem/concern/cli'
-require 'ylem/concern/output'
+require_relative '../cli'
+require_relative '../output'
 
 # ``Cli`` outputs SHOULD be programmatically deactivable,
 # for testing purpose. This concern provides an ``attr_accessor`` to do so.
 module Ylem::Concern::Cli::Output
-  extend ActiveSupport::Concern
   include Ylem::Concern::Output
 
   # @!attribute [rw] quiet
   #   Allow/deny access to outputs (``STDOUT``, ``STDERR``)
   #   @return [Boolean]
   #   @see Ylem::Concern::Output
-  included { attr_accessor :quiet }
+  class << self
+    def included(base)
+      base.class_eval <<-"ACCESSORS", __FILE__, __LINE__ + 1
+        attr_accessor :quiet
+      ACCESSORS
+    end
+  end
+
+  # included { attr_accessor :quiet }
 
   # Denote quiet
   #
