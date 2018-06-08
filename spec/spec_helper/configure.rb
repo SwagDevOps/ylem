6155cfd750a127b079109e1dc9d0c2c2476bf54b
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-08 09:16:06 +0200

    spec_helper/configure (spec) factory_bot removal

--- spec/spec_helper/configure.rb
+++ spec/spec_helper/configure.rb
@@ -7,15 +7,3 @@ require_relative 'local'
 Sham::Config.activate!(Pathname.new(__dir__).join('..').realpath)
 
 Object.class_eval { include Local }
-
-require 'factory_bot'
-FactoryGirl = FactoryBot
-
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-  FactoryGirl.find_definitions
-
-  def build(name)
-    FactoryGirl.build(name)
-  end
-end
