7b1485260d1a2b422480a48a7e535dfe2996d549
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-08 18:09:19 +0200

    VERSION introduced

---
+++ lib/ylem/VERSION.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+# rubocop:disable Style/Documentation
+
+module Ylem
+  require_relative 'version'
+
+  VERSION = Ylem::Version.new.freeze
+end
+
+# rubocop:enable Style/Documentation
