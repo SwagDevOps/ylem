065e13363797511a96b5569818e9f804cc2bf98d
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-10 16:54:26 +0200

    system (helper) require_relative

--- lib/ylem/helper/system.rb
+++ lib/ylem/helper/system.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'ylem/helper'
-require 'ylem/concern/helper'
+require_relative '../helper'
+require_relative '../concern/helper'
 require 'sys/proc'
 
 # System helper
