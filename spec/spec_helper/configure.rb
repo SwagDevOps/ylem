59d282e862672602a73404d61c38f80a50e00184
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-12 00:52:31 +0200

    spec_helper/configure (spec) RuntimeError for "Unhandled SystemExit"

--- spec/spec_helper/configure.rb
+++ spec/spec_helper/configure.rb
@@ -7,3 +7,14 @@ require_relative 'local'
 Sham::Config.activate!(Pathname.new(__dir__).join('..').realpath)
 
 Object.class_eval { include Local }
+
+RSpec.configure do |rspec|
+  # @see https://github.com/rspec/rspec-core/issues/2246
+  rspec.around(:example) do |example|
+    begin
+      example.run
+    rescue SystemExit => e
+      raise "Unhandled SystemExit (#{e.status})"
+    end
+  end
+end
