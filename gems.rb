85d1ac92121e12b11ca7ec4cf17e4719ca0907a0
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-25 11:00:07 +0200

    development (gems) as a single group

--- gems.rb
+++ gems.rb
@@ -15,23 +15,23 @@ group :development do
   gem 'kamaze-project', '~> 1.0', '>= 1.0.3'
   gem 'listen', '~> 3.1'
   gem 'rubocop', '~> 0.56'
-end
 
-group :development, :repl do
-  gem 'interesting_methods', '~> 0.1'
-  gem 'pry', '~> 0.11'
-  gem 'pry-coolline', '~> 0.2'
-end
+  group :repl do
+    gem 'interesting_methods', '~> 0.1'
+    gem 'pry', '~> 0.11'
+    gem 'pry-coolline', '~> 0.2'
+  end
 
-group :development, :doc do
-  gem 'github-markup', '~> 2.0'
-  gem 'redcarpet', '~> 3.4'
-  gem 'yard', '~> 0.9'
-end
+  group :doc do
+    gem 'github-markup', '~> 2.0'
+    gem 'redcarpet', '~> 3.4'
+    gem 'yard', '~> 0.9'
+  end
 
-group :development, :test do
-  gem 'fuubar', '~> 2.3'
-  gem 'hashie', '~> 3.5'
-  gem 'rspec', '~> 3.6'
-  gem 'sham', '~> 2.0'
+  group :test do
+    gem 'fuubar', '~> 2.3'
+    gem 'hashie', '~> 3.5'
+    gem 'rspec', '~> 3.6'
+    gem 'sham', '~> 2.0'
+  end
 end
