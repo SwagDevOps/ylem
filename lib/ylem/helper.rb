a84c6479093df1f167d3982b6aba8ba895f2ec96
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-07-05 15:58:15 +0200

    helper does not mask real error

--- lib/ylem/helper.rb
+++ lib/ylem/helper.rb
@@ -9,18 +9,12 @@ class Ylem::Helper
 
   # @param [String|Symbol] name
   # @return [Object]
-  #
-  # @raise [NotImplementedError]
   def get(name)
     name = name.to_sym
 
     return items[name] if items[name]
 
-    begin
-      @items[name] = inflector.resolve("ylem/helper/#{name}").new
-    rescue LoadError
-      raise NotImplementedError, "helper not loadable: #{name}"
-    end
+    @items[name] = inflector.resolve("ylem/helper/#{name}").new
   end
 
   protected
