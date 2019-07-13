17aff962285f6a0e16e6787d95ec2008bf540fd7
Dimitri Arrigoni <dimitri@arrigoni.me>
2019-07-13 16:08:04 +0200

    apply license headers on lib files

--- lib/ylem/concern/timed_output.rb
+++ lib/ylem/concern/timed_output.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Copyright (C) 2017-2019 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
 require_relative '../concern'
 require_relative 'helper'
 
