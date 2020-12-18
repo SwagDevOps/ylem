1882304cdd7998da0860e6c3f574744731014594
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-12-18 16:41:58 +0100

    gems replace nested groups

--- gems.rb
+++ gems.rb
@@ -15,21 +15,21 @@ group :development do
   gem 'kamaze-project', '~> 1.0', '>= 1.0.3'
   gem 'listen', '~> 3.1'
   gem 'rubocop', '~> 0.74'
+end
 
-  group :repl do
-    gem 'interesting_methods', '~> 0.1'
-    gem 'pry', '~> 0.12'
-    gem 'pry-coolline', '~> 0.2'
-  end
+group :repl do
+  gem 'interesting_methods', '~> 0.1'
+  gem 'pry', '~> 0.12'
+  gem 'pry-coolline', '~> 0.2'
+end
 
-  group :doc do
-    gem 'github-markup', '~> 3.0'
-    gem 'redcarpet', '~> 3.5'
-    gem 'yard', '~> 0.9'
-  end
+group :doc do
+  gem 'github-markup', '~> 3.0'
+  gem 'redcarpet', '~> 3.5'
+  gem 'yard', '~> 0.9'
+end
 
-  group :test do
-    gem 'rspec', '~> 3.8'
-    gem 'sham', '~> 2.0'
-  end
+group :test do
+  gem 'rspec', '~> 3.8'
+  gem 'sham', '~> 2.0'
 end
